70986ed5a109261c19eec7578d940373173d1b11
appie999 <aitoutmghartfarid@gmail.com>
2025-01-19 15:06:13 +0100

    add function to get total

---
+++ script.js
@@ -0,0 +1,75 @@
+
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+
+
+
+
+//get total
+
+function getTotal(){
+     
+    if(price.value != ''){
+        let result = (+price.value + +taxes.value + +ads.value) - +discount.value
+        total.innerHTML = result;
+        total.style.background ='rgb(3, 145, 31)'
+    }else{
+        total.innerHTML=''
+        total.style.background ='rgb(191, 33, 33)'
+    }
+}
+
+
+//create product
+
+let dataProduct = [];
+
+
+submit.onclick = function(){
+    let newProduct = {
+        title:title.value,
+        price:price.value,
+        taxes:taxes.value,
+        ads:ads.value,
+        discount:discount.value,
+        total:total.innerHTML,
+        count:count.value,
+        category:category.value,
+    }
+    console.log(newProduct);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//save LocalStorage
+//clear data
+//read
+//count
+//delete
+//update
+// search
+//clean data
